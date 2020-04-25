1df730b5784e37afea780e2fcf0f0e169d831908
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-04-25 14:19:19 +0530

    Added XOR Cipher

--- cipher.py
+++ cipher.py
@@ -1,24 +1,7 @@
-"""
-cipher.py implements CaesarCipher for encrypting and decrypting login password.
+import lib
 
-The functions below turns each character of the given text
-into an ASCII value and then changes the value by the
-given key. In this way the text is encrypted and decrypted based
-upon the shifting of the characters.
-"""
-
-
-"""enc() encrypts the given plain text using the given key."""
-def enc(text, key):
-    enc_text = ""
-    for itr in range(0, len(text)):
-        enc_text = enc_text + chr(ord(text[itr]) + key)
-    return enc_text
-
-
-"""dec() deciphers the encrypted text into plain text if the passed key is correct."""
-def dec(text, key):
-    dec_text = ""
-    for itr in range(0, len(text)):
-        dec_text = dec_text + chr(ord(text[itr]) - key)
-    return dec_text
+def enc_dec(text, key):
+    enc_dec_text = ""
+    for encrypt in range(0, len(text)):
+        enc_dec_text = enc_dec_text + chr(lib.to_dec(lib.xor(ord(text[encrypt]), key)))
+    return enc_dec_text
