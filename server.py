64ba215d4d6ad7a1061c5e3a48c821117c1636cd
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-05-24 22:26:12 +0530

    Server code

--- server.py
+++ server.py
@@ -1,28 +1,19 @@
 from flask import Flask
-from requests import get
 import memory_structures as ms
 
-def get_data(ip, memory):
-    address = "http://" + ip + ":5000/" + memory
-    data = get(address)
-    return data.text
+server = Flask(__name__)
 
-def launch_server():
-    server = Flask(__name__)
+@server.route("/")
+def index():
+    return "hey."
 
-    @server.route("/")
-    def index():
-        return "hey."
-
-    @server.route("/mess")
-    def mess():
-        return ms.mess
+@server.route("/mess")
+def mess():
+    return open("mess_file.txt", "r").read()
     
-    @server.route("/cluster")
-    def cluster():
-        return ms.cluster
+@server.route("/cluster")
+def cluster():
+    return open("cluster_file.txt", "r").read()
     
-    if __name__ == "__main__":
-        server.run(debug = True, host = "0.0.0.0")
-
-launch_server()
+if __name__ == "__main__":
+    server.run(debug = True, host = "0.0.0.0")
