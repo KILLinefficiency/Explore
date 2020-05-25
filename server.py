bb809618a1a8b027279198c9b58152afd1f62be3
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-05-25 22:37:09 +0530

    added index page

--- server.py
+++ server.py
@@ -3,17 +3,27 @@ import memory_structures as ms
 
 server = Flask(__name__)
 
+index_page = """
+<h1 align = 'center'>Explore Server</h1>
+<h3>Raw Data can be found at the following routes of this IP address:</h3>
+<h3><code>/mess</code></h3>
+
+<h3><code>/cluster</code></h3>
+"""
+
 @server.route("/")
 def index():
-    return "hey."
+    return index_page
 
 @server.route("/mess")
 def mess():
-    return open("mess_file.txt", "r").read()
+    mess_raw_data = open("mess_file.txt", "r", encoding = "utf-8")
+    return mess_raw_data.read()
     
 @server.route("/cluster")
 def cluster():
-    return open("cluster_file.txt", "r").read()
+    cluster_raw_data = open("cluster_file.txt", "r", encoding = "utf-8")
+    return cluster_raw_data.raw()
     
 if __name__ == "__main__":
     server.run(debug = True, host = "0.0.0.0")
