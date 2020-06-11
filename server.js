4d7130191905559070044e433e92776addb2d8c3
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-06-11 23:11:39 +0530

    used async methods and err catcher

--- server.js
+++ server.js
@@ -1,24 +1,61 @@
 const http = require("http");
-const fs = require("fs")
+const fs = require("fs");
+
+const SERVER_PORT = 2166;
+
+const index_page = `<html>
+<head>
+	<title>Explore Server</title>
+</head>
+
+<body>
+<h1 align = "center">Explore Sever</h1>
+<br>
+<h3>Routes for Explore Server:</h3>
+<code>/mess</code>
+<br><br>
+<code>/cluster</code>
+</body>
+
+</html>
+`;
+
+const mess_file = ".mess_server_file.txt";
+const cluster_file = ".cluster_server_file.txt";
 
 server = http.createServer((req, res) => {
 	if(req.url == "/") {
-		res.write("you know the rules and so do i...");
+		res.write(index_page);
 		res.end();
 	}
 	if(req.url == "/mess") {
-		mess_content = fs.readFileSync("mess_file.txt");
-		mess = mess_content.toString();
-		res.write(mess);
-		res.end();
+		fs.readFile(mess_file, (err, mess_contents) => {
+			if(err) {
+				console.log("Error: Update the server from your Explore instance.")
+			}
+			else {
+				res.write(mess_contents);
+				res.end();
+			}
+		});
 	}
 	if(req.url == "/cluster") {
-		cluster_content = fs.readFileSync("cluster_file.txt");
-		cluster = cluster_content.toString();
-		res.write(cluster);
-		res.end();
+		fs.readFile(cluster_file, (err, cluster_contents) => {
+			if(err) {
+				console.log("Error: Update the server from your Explore instance.")
+			}
+			else {
+				res.write(cluster_contents);
+				res.end();
+			}
+		});
 	}
 });
 
-server.listen(2166, "0.0.0.0", () => { console.log("Server running on port 2166"); });
+var reqs = 1;
+server.on("connection", (socket) => {
+	console.log(`Request recieved... (${reqs})`);
+	reqs = reqs + 1;
+});
 
+server.listen(SERVER_PORT, "0.0.0.0", () => { console.log(`\nServer running on port ${SERVER_PORT}...\n`); });
