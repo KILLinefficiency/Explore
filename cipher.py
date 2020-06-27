d695b8cadb32b5872bb68a109b9d78a534d2cfcd
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-06-27 21:31:48 +0530

    used ^ operator

--- cipher.py
+++ cipher.py
@@ -3,5 +3,5 @@ import lib
 def enc_dec(text, key):
     enc_dec_text = ""
     for encrypt in range(0, len(text)):
-        enc_dec_text = enc_dec_text + chr(lib.to_dec(lib.xor(ord(text[encrypt]), key)))
+        enc_dec_text = enc_dec_text + chr(ord(text[encrypt]) ^ key)
     return enc_dec_text
