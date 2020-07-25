10d3cf9d39d1b1ae0ebea9378a44faa55c188345
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-07-26 02:40:27 +0530

    handled an error

--- shell.py
+++ shell.py
@@ -669,7 +669,7 @@ while flag:
     except KeyboardInterrupt:
         print("\n\nBye.")
         break
-    # except:
-        # print("-1")
-        # continue
+    except:
+        print("-1")
+        continue
 log.close()
